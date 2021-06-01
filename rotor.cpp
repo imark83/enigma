8ce5dd2caddefa4061190cf0e1f81f40032d47c5
Marcos Rodríguez <marcos@unizar.es>
2021-06-01 12:53:04 +0200

    minimos cambios

--- rotor.cpp
+++ rotor.cpp
@@ -17,8 +17,8 @@ public:
     //this->scrambling = std::vector<unsigned int>(3,4,5);
     switch (rotNumber) {
       case 1: this->scrambling = 
-        {20,22,24,03,22,24,25,08,13,16,17,19,16,23,
-         24,04,Q,R,S,T,U,V,W,X,Y,Z};
+        {20,22,24,03,22,24,25,08,13,16,17,19,16,
+         23,24,04,17,06,00,18,23,13,17,19,16,10};
   }
 
 
@@ -33,6 +33,10 @@ public:
 int main(int argc, char const *argv[]) {
   
   Rotor a(1,2);
+  for(int i=0; i<26; ++i) {
+    char 
+    std::cout << 'a'+rot.scrambling[i];
+  }
 
   return 0;
 }
