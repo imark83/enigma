bcc6d10214d2f3322f22f6220880fa947dc24371
Marcos Rodríguez <marcos@imark.es>
2021-06-16 13:17:26 +0200

    stalled! :/

--- rotor.hpp
+++ rotor.hpp
@@ -35,6 +35,24 @@ public:
   
 };
 
+Rotor & operator=(Rotor &op1, Rotor &op2) {
+  op1.rotNumber=op2.rotNumber;    // ROTOR ID. NOT USED IN COMPUTING
+  op1.ringstellung=op2.ringstellung;           // RINGSTELLUNG
+
+  op1.display=op2.display;      // DISPLAYED LETTER
+  op1.tick_label=op2.tick_label;
+
+
+
+  op1.scram = op2.scram; // FORWARD SCRAMBLING, 
+      // LEFT TO RIGHT
+
+  op1.backscram = op2.backscram; // BACKWARD SCRAMBLING, 
+      // RIGHT TO LEFT
+
+
+  return op1;
+}
 
 class Reflector {
 public:
